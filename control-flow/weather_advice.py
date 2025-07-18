643ecbcfb31620968bc7604fd7bfe02e3e48ef28
Brian8933 <aduwob@gmail.com>
2025-07-19 01:06:25 +0300

    Fix: adjust conditional

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,11 +2,7 @@
 
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if weather == "sunny":
-   print("wear a t-shirt and sun glasses. ")
-elif weather == "rainy":
-   print("Don't forget your umbrella and a raincoat. ")
-elif weather == "cold":
-   print("Make sure you wear warm coat and a scarf. ")
-else:
-   print("Sorry, I don't have recommendation for this weather")       
+if weather == "sunny": print("wear a t-shirt and sun glasses. ")
+elif weather == "rainy": print("Don't forget your umbrella and a raincoat. ")
+elif weather == "cold": print("Make sure you wear warm coat and a scarf. ")
+else: print("Sorry, I don't have recommendation for this weather")        
