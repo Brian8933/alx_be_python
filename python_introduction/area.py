854018622effefa283defb6efb2ef17bbca77bbe
Brian8933 <aduwob@gmail.com>
2025-07-26 11:52:38 +0300

    Add area.py

---
+++ python_introduction/area.py
@@ -0,0 +1,7 @@
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+# test the functions
+result = calculate_area(5, 3)
+print(f"The area of the rectangle is: {result}")
