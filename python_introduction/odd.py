bcb0d9d5e80f624bca1e170460b9c52b163a7de5
Brian8933 <aduwob@gmail.com>
2025-07-26 11:53:32 +0300

    Add odd.py

---
+++ python_introduction/odd.py
@@ -0,0 +1,9 @@
+def check_even_number(number):
+    if number % 2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd")   
+
+# Example usage:
+check_even_number(4)
+check_even_number(7)        
